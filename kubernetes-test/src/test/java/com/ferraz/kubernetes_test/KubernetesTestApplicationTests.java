6ecec4c23d583880905bc6c19bda35c79e64600c
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2025-03-10 11:22:55 -0300

    adiciona primeiro projeto

---
+++ kubernetes-test/src/test/java/com/ferraz/kubernetes_test/KubernetesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferraz.kubernetes_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
